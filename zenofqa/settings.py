6614627ffa6f7924314f9d6428290a0823dba7ab
devin <zhangchuzhao@youmi.net>
2020-11-23 20:49:59 +0800

    chore: 关闭debug模式

--- zenofqa/settings.py
+++ zenofqa/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '05^q*w7p!-dkz==dis3cx)93m6=i@f60#+r_bkypmjx2p9pg=r'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '81.69.160.149', '.zenqa.com']
 
