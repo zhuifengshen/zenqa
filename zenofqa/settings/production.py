08fbe288584ec0f7decb0679acb9de56c447fbd6
Devin <zhangchuzhao@dingtalk.com>
2023-12-06 20:20:22 +0800

    Update production.py

--- zenofqa/settings/production.py
+++ zenofqa/settings/production.py
@@ -2,7 +2,8 @@ from .common import *
 
 SECRET_KEY = '05^q*w7p!-dkz==dis3cx)93m6=i@f60#+r_bkypmjx2p9pg=r'
 DEBUG = False
-ALLOWED_HOSTS =  ['127.0.0.1', 'localhost', '81.69.160.149', '.zenqa.com']
+# ALLOWED_HOSTS =  ['127.0.0.1', 'localhost', '81.69.160.149', '.zenqa.com']
+ALLOWED_HOSTS = ['*']
 CACHES = {
   'default': {
     
